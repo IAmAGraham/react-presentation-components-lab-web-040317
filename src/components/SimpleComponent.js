462c8878d722c729c19a8961413344616be2996b
github username <github email address>
2017-06-09 09:06:03 -0400

    simple, simplier and odd

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
-// Code SimpleComponentHere Here
+import React, {Component} from "react";
+
+export default class SimpleComponent extends Component {
+  constructor(){
+    super();
+    this.state = {
+      mood: "happy"
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event){
+    this.setState({
+      mood: this.state.mood === "happy" ? "sad" : "happy"
+    })
+  }
+
+  render(){
+    return(
+      <div onClick={this.handleClick}>
+        <p>{this.state.mood}</p>
+      </div>
+    )
+  }
+}
